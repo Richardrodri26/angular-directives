2567ff75385a40cb61c296f51220e25a1cad2f3a
RichardRodri26 <richardmanuel811@gmail.com>
2025-05-07 21:03:09 -0500

    Add error handling to customLabel directive and bind errors in product page

--- src/app/shared/directives/customLabel.directive.ts
+++ src/app/shared/directives/customLabel.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]',
@@ -6,12 +7,18 @@ import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 export class CustomLabelDirective implements OnInit {
 
   private htmlElement?: ElementRef<HTMLElement>;
-  private _color: string = 'red'
+  private _color: string = 'red';
+  private _errors?: ValidationErrors | null = null;
 
   @Input() set color(value: string) {
     this._color = value;
 
     this.setStyle();
+  }
+
+  @Input() set errors(value: ValidationErrors | null | undefined) {
+    this._errors = value;
+    this.setErrorMessage();
   }
 
   constructor(
@@ -29,5 +36,31 @@ export class CustomLabelDirective implements OnInit {
     if(!this.htmlElement) return;
     this.htmlElement!.nativeElement.style.color = this._color
   }
+
+  setErrorMessage(): void {
+    if(!this.htmlElement) return;
+    if(!this._errors) {
+      this.htmlElement!.nativeElement.innerText = ''
+      return;
+    }
+
+    const errors = Object.keys(this._errors);
+
+    if (errors.includes('required')) {
+      this.htmlElement!.nativeElement.innerText = 'Requerido';
+    }
+
+    if (errors.includes('minlength')) {
+      const min = this._errors?.['minlength'].requiredLength;
+      const current = this._errors?.['minlength'].actualLength;
+      this.htmlElement!.nativeElement.innerText = `Mínimo ${min} caracteres, actualmente ${current}`;
+    }
+
+    if (errors.includes('email')) {
+      this.htmlElement!.nativeElement.innerText = 'Email inválido';
+    }
+
+  }
+
 
 }
